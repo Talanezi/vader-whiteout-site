c03c7aca5baa292222dd3de82c9b7e2a12278fc6
Talanezi <talanezi@ucsd.edu>
2026-04-01 13:29:22 -0700

    Update config.js

--- scheduler/config.js
+++ scheduler/config.js
@@ -1,1 +1,40 @@
+window.PRODUCTION_CONFIG = {
+  supabaseUrl: "https://YOUR_PROJECT_ID.supabase.co",
+  supabaseAnonKey: "YOUR_SUPABASE_ANON_PUBLIC_KEY",
 
+  appName: "Vader: Whiteout Production Scheduler",
+  publicHomeUrl: "https://vaderwhiteout.com",
+  membersUrl: "https://vaderwhiteout.com/members/",
+  timezone: "America/Los_Angeles",
+
+  roleWeights: {
+    "Executive Producer": 5,
+    "Director": 5,
+    "Producer": 4,
+    "Director of Photography": 5,
+    "Fight Choreographer": 4,
+    "1st Assistant Director": 3,
+    "2nd Assistant Director": 2,
+    "Anakin": 5,
+    "Darth Vader": 5,
+    "Ada": 3,
+    "Torin": 3,
+    "Support Cast": 2,
+    "Camera Operator": 2,
+    "Storyboard Artist": 1,
+    "Gaffer": 3,
+    "Production Designer": 3,
+    "Costume Designer": 2,
+    "Makeup Artist": 2,
+    "Assistant": 1
+  },
+
+  preferenceScores: {
+    available: 2,
+    maybe: 1,
+    unavailable: 0
+  },
+
+  requiredAvailableBonus: 4,
+  requiredMaybeBonus: 2
+};
